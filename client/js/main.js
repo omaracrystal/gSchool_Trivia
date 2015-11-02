f2d0f4b609907722c85d5406416b64c2872c9481
Crystal <crystal@Zachs-MacBook-Air.local>
2015-11-01 21:35:24 -0700

    fixed question add form

--- client/js/main.js
+++ client/js/main.js
@@ -11,60 +11,6 @@ $("#gitUserName").hide();
 //DOCUMENT ready
 });
 
-////////////////////question bank////////////////////
-var qBank = [
-  {
-  //set1
-    category: "JavaScript",
-    question: "What does the following expression return? <br> 3 / 'bob';",
-    choices: ["undefined", "ReferenceError", "NaN"],
-    answer: "NaN"
-  }, {
-  //set2
-    category: "JavaScript",
-    question: "What is a method?",
-    choices: ["Used to describe an object.", "A function assigned to an object.", "Performs a function on one or more operands or variables."],
-    answer: "A function assigned to an object."
-  }, {
-  //set3
-    category: "JavaScript",
-    question: "Which company first implemented the JavaScript language?",
-    choices: ["Netscape Communications Corp.", "Microsoft Corp.", " Sun Microsystems Corp."],
-    answer: "Netscape Communications Corp."
-  }, {
-    category: "CSS",
-    question: "When was the first release of a browser supporting JavaScript?",
-    choices: ["1996", "1995", " 1994"],
-    answer: "1995"
-  }, {
-    category: "CSS",
-    question: "The original name of JavaScript was?",
-    choices: ["LiveScript", "ECMAScript", "WireScript"],
-    answer: "LiveScript"
-  }, {
-    category: "CSS",
-    question: "Which of the following browsers was the first to support JavaScript?",
-    choices: ["Microsoft Internet Explorer 2.0 beta", "Netscape Navigator 2.0 beta", "Opera 2.0 beta"],
-    answer: "Netscape Navigator 2.0 beta"
-  }, {
-    category: "HTML",
-    question: "Applying a function to a value and creating a new value is called what?",
-    choices: ["declaration", "projection", "expression"],
-    answer: "projection"
-  }, {
-    category: "HTML",
-    question: "Another question?",
-    choices: ["1st answer", "2nd answer", "real answer"],
-    answer: "real answer"
-  }, {
-    category: "HTML",
-    question: "question?",
-    choices: ["1st answer", "2nd answer", "real answer"],
-    answer: "real answer"
-  }
-]
-////////////////////end of question bank////////////////////
-
 ////////////////////Utilities////////////////////
 //shuffle function for question bank
 function shuffle(array) {
@@ -141,7 +87,7 @@ $("#add").on("click", function (e){
   var addWrong2 = $('input:eq(3)').val();
 
   //create a new object for qBank array
-  var addQuestion = {category: addCategory, question: addQuestion, answer: addAnswer, choices: "["+addWrong1+","+addWrong2+","+addAnswer+"]"}
+  var addQuestion = {category: addCategory, question: addQuestion, answer: addAnswer, choices: [addWrong1,addWrong2, addAnswer]}
 
   //push new questions to qBank
   qBank.push(addQuestion);
